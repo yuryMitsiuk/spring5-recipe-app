5e9c042eea53f476f40fb2345ae4c14e2e3291d4
Yury Mitsiuk <yriymitsuik@gmail.com>
2018-09-21 20:44:00 +0300

    using mockito mocks

---
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,38 @@
+package guru.springframework.spring5recipeapp.services;
+
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+    private RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getAll() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipes = new HashSet<>();
+        recipes.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipes);
+        assertEquals(recipeService.getAll().size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
