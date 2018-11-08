0d0bfee40c6c949f47cd1cce922553c06c7dd4ca
Yury Mitsiuk <yriymitsuik@gmail.com>
2018-11-08 18:01:09 +0300

    show ingredients

---
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IngredientController.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5recipeapp.controllers;
+
+import guru.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{recipeId}/ingredients")
+    public String showAll(@PathVariable String recipeId, Model model) {
+        log.debug("Getting ingredient list for recipe id: " + recipeId);
+
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+        return "recipe/ingredient/list";
+    }
+}
